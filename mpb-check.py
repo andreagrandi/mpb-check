3c5ab3c3134f71043d3c916206ece624631c9a40
Andrea Grandi <a.grandi@gmail.com>
2018-08-16 06:54:26 +0100

    Add code to send an email

--- mpb-check.py
+++ mpb-check.py
@@ -1,5 +1,21 @@
 import scrapy
 import os
+import sendgrid
+from sendgrid.helpers.mail import Email, Content, Mail
+
+
+def send_email(quantity):
+    sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
+    from_email = Email("info@sendgrid.com")
+    to_email = Email(os.environ.get('EMAIL_TO_NOTIFY'))
+    subject = "MPB Check: the item you are monitoring is available"
+    message = 'There are {0} items available at {1}'.format(quantity, os.environ.get('MPB_URL'))
+    content = Content("text/plain", message)
+    mail = Mail(from_email, subject, to_email, content)
+    response = sg.client.mail.send.post(request_body=mail.get())
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
 
 
 class MBPSpider(scrapy.Spider):
@@ -16,7 +32,6 @@ class MBPSpider(scrapy.Spider):
         quantity = int(response.css('strong.www-results-count::text').extract_first())
 
         if quantity > 0:
-            # Send an email
-            pass
+            send_email(quantity)
 
         self.log('Quantity available: %s' % quantity)
